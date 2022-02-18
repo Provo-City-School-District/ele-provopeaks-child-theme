475fa59d3e5f2389f2cf7a7b715699233afd9e19
Espi <joshespi@gmail.com>
2022-02-18 13:28:42 -0700

    modified the location of the Teacher Access flyout

--- peaks-child/assets/js/child-scripts.js
+++ peaks-child/assets/js/child-scripts.js
@@ -7,8 +7,8 @@ jQuery(".teacherActivate").click(function() {
 	return false;
 });
 jQuery(".teacherActivate").click(function() {
-		if(jQuery("#teacherAccess").css("margin-top") != "-290px") {
-			jQuery("#teacherAccess").css("margin-top", "-290px");
+		if(jQuery("#teacherAccess").css("margin-top") != "-344px") {
+			jQuery("#teacherAccess").css("margin-top", "-344px");
 			jQuery("#teacherAccess ul").css("display", "block");
 		} else {
 			jQuery("#teacherAccess").css("margin-top", "-38px");
